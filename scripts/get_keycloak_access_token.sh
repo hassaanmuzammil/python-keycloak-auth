a58b7adbbc32a3c07ce47342ce2d5ccc931d3e3f
hassaanmuzammil <hassaanmuzammil97@hotmail.com>
2025-05-24 13:30:08 +0500

    update keycloak sh scripts

--- scripts/get_keycloak_access_token.sh
+++ scripts/get_keycloak_access_token.sh
@@ -1,27 +1,18 @@
-# source ../.env &&
+#!/bin/bash
+# This script retrieves an access token from Keycloak using either client credentials or password grant type.
 
+# Using client credentials grant type. Retrieves an access token for a client.
 # curl -X POST "http://localhost:8080/realms/realm-1/protocol/openid-connect/token" \
 #      -H "Content-Type: application/x-www-form-urlencoded" \
 #      -d "client_id=client-1" \
 #      -d "client_secret=I0wVz0OuX1V5zPBHppkhXJYkrqIdOdE2" \
 #      -d "grant_type=client_credentials"
 
-# curl -X POST "http://localhost:8080/realms/master/protocol/openid-connect/token" \
-#      -H "Content-Type: application/x-www-form-urlencoded" \
-#      -d "client_id=admin-cli" \
-#      -d "username=admin" \
-#      -d "password=admin" \
-#      -d "grant_type=password"
-
+# Using password grant type with a specific user. Retrieves an access token for a user.
 # curl -X POST "http://localhost:8080/realms/realm-1/protocol/openid-connect/token" \
-#      -H "Content-Type: application/x-www-form-urlencoded" \
-#      -d "client_id=client-1" \
-#      -d "username=email-1@gmail.com" \
-#      -d "password=password-1" \
-#      -d "grant_type=password"
-
-# curl -X POST "$KEYCLOAK_SERVER_URL/realms/$KEYCLOAK_REALM/protocol/openid-connect/token" \
 #      -H "Content-Type: application/x-www-form-urlencoded" \
 #      -d "client_id=$KEYCLOAK_CLIENT" \
 #      -d "client_secret=$KEYCLOAK_CLIENT_SECRET" \
-#      -d "grant_type=client_credentials"
+#      -d "username=email-1@gmail.com" \
+#      -d "password=password-1" \
+#      -d "grant_type=password"
